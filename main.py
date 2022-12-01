7f447705d0e874e6d035f996339f7a0572f502c7
ivkrak <iv.krak@gmail.com>
2022-12-01 14:43:29 +0800

    Создан файл functions.py для выноса лишних функций

--- main.py
+++ main.py
@@ -1,36 +1,6 @@
-# import telethon
-
-
-def data_for_chat_parsing():
-    """
-    Функция для приёма:
-
-    **user_API**
-
-    **списка ссылок на чаты**
-
-    :return: Список ссылок на чаты, user_API
-    """
-    arr_chats = []
-    user_API = input('Напишите сюда свой user API - ')
-    n = int(input("""Для выбора одного из сценариев нужно ввести число:
-        1 - Построчный ввод ссылок на чаты
-        0 - Ввод ссылок на чаты через ; (точку с запятой)
-        
-        Ваше число - """))
-
-    match n:
-        case 1:
-            print('Для завершения ввода отправьте пустой ввод')
-            while True:
-                s = input('Напишите сюда название чата - ')
-                if s == '':
-                    break
-                else:
-                    arr_chats.append(s)
-        case 0:
-            arr_chats = input('Напишите сюда название чатов через ;  (точку с запятой) - ').replace(' ', '').split(';')
-    return arr_chats, user_API
+from telethon import TelegramClient
+from API import api_id, api_hash
+from functions import data_for_chat_parsing
 
 
 arr_chats, user_API = data_for_chat_parsing()
