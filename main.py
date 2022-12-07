38a1e474f35d667e037d2989268da584635f44a3
ivkrak <iv.krak@gmail.com>
2022-12-08 01:35:46 +0800

    1

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import warnings
 from telethon.tl.functions.channels import JoinChannelRequest
 import socks
 from telethon.tl.functions.users import GetFullUserRequest
-import time
+from datetime import datetime
 from tqdm import tqdm
-
+import requests
+from bs4 import BeautifulSoup
 def filter_words(words_list, text):
     """
     Возвращает False, если в text нет ни одного слова из words_list, в остальных случаях возвращает True
@@ -21,9 +22,19 @@ def filter_words(words_list, text):
     return False
 
 
-async def get_bio(user):
-    pass
+def get_bio(user_login):
+    """
+    Функция для выполнения запросов (about user)
+    :param user_login: имя пользователя / username
+    :return: информация о пользователе типа str
+    """
+    try:
+        r = requests.get(f'https://t.me/{user_login}')
+        arr = list(r.text.split('\n'))
+        s = str((arr[27])[42::])
 
+        return s if 'You can contact' not in s else None
+    except: return None
 
 client = TelegramClient('79263782950', api_id='2040', api_hash='b18441a1ff607e10a989891a5462e627',
                         proxy=(3, "gate.dc.smartproxy.com", 20000, True, "user-Port50", "Ghd7lKaQj077"))
@@ -32,9 +43,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 def date_about_chat_users(url_list):
-    num_of_users = 0
     df = pd.DataFrame()
-    df2 = pd.DataFrame()
     for url in url_list:
         print(f'Собираю информацию из чата: {url}')
 
@@ -44,10 +53,12 @@ def date_about_chat_users(url_list):
             except Exception as ex: print(ex)
             participants = client.get_participants(url)
             for user in tqdm(participants):
-                num_of_users += 1
-                if not (user.username is None):
-                    bio = client(GetFullUserRequest(user)).full_user.about
-                    if not (user.username is None) and not (bio is None):
+                print(user)
+                was_online = user.status.was_online
+
+                if user.username is not None:
+                    bio = get_bio(user.username)
+                    if user.username is not None and bio is not None:
                         new_dct = {
                             "User ID": user.id,
                             "User name": user.username,
@@ -57,23 +68,47 @@ def date_about_chat_users(url_list):
                             "Premium": user.premium,
                             "About user": bio
                         }
-                        if filter_words(words_list=['smm', 'смм'], text=str(new_dct) + str(bio)):
-                            # print('----Пользователь прошёл фильтр и был добавлен в таблицу----')
+                        if True or filter_words(words_list=['smm', 'смм'], text=str(new_dct) + str(bio)):
                             df = df.append(new_dct, ignore_index=True)
                             df.reset_index(drop=True, inplace=True)
         except Exception as ex:
             print(f'{ex}\nОШИБКА С ЧАТОМ: {url}')
     df.rename(columns={"": "index"})
-    df2.rename(columns={"": "index"})
     df.to_csv('Data/Filtered_Users_info.csv')
-    df2.to_csv('Data/All_Users_info.csv')
-    print(f'Количество проскандированных пользователей: {num_of_users}')
 
 
 if __name__ == '__main__':
-    t = time.time()
+    t = datetime.now()
     date_about_chat_users([
-
+    'https://t.me/chat3'
     ])
-    print("---%s seconds---" % (time.time() - t))
+    print('Время работы программы: ', datetime.now() - t)
 
+User(
+    id=5215628722,
+    is_self=False,
+    contact=False,
+    mutual_contact=False,
+    deleted=False,
+    bot=False,
+    bot_chat_history=False,
+    bot_nochats=False,
+    verified=False,
+    restricted=False,
+    min=False,
+    bot_inline_geo=False,
+    support=False,
+    scam=False,
+    apply_min_photo=True,
+    fake=False,
+    bot_attach_menu=False,
+    premium=False,
+    attach_menu_enabled=False,
+    access_hash=-4505735046168541241,
+    first_name='Ngm',
+    last_name=None,
+    username=None,
+    phone=None,
+    photo=None,
+    status=UserStatusOffline(
+        was_online=datetime.datetime(2022, 12, 7, 11, 46, 43, tzinfo=datetime.timezone.utc)), bot_info_version=None, restriction_reason=[], bot_inline_placeholder=None, lang_code=None, emoji_status=None, usernames=[])
